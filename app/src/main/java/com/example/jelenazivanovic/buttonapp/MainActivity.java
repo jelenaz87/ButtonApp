1d2ade3de12fa741c84412147b8529befffc3137
ENDAVA\jelena.zivanovic <jelena.zivanovic@endava.com>
2017-11-03 15:13:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jelenazivanovic/buttonapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.jelenazivanovic.buttonapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       mButton = (Button) findViewById(R.id.button1);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mButton.setText("kliknuto");
+                mButton.setBackgroundColor(Color.BLUE);
+            }
+        });
+
+        mButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mButton.setBackgroundColor(Color.RED);
+                return false;
+            }
+        });
+
+        mButton.setVisibility(View.INVISIBLE);
+
+    }
+}
